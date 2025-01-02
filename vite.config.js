ac1089fb44c3f3663f3c2fb4b4672a5f71b5939f
skmaurya08 <emailsshubham@gmail.com>
2025-01-02 11:09:30 +0530

    first1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to 0.0.0.0 to allow external access
+    port: process.env.PORT || 5173, // Use the PORT environment variable or default to 5173
+  },
 })
